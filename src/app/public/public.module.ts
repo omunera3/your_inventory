6cddafd9ed6b8277a646c416dec1202d2fe78320
Oscar Alarcón <oscarmunera37@gmail.com>
2023-12-12 06:49:54 -0500

    Se crean carpetas y la estructura base para que el proyecto sea escalable

---
+++ src/app/public/public.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../core/shared/shared.module';
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+  ]
+})
+export class PublicModule { }
