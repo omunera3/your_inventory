0cc549276ff8e869612f97e088bb4173d629b05a
Oscar Alarcón <omunera@openmytab.com>
2024-02-06 23:09:00 -0500

    Se crea metodo para agregar filas con los requerimientos para agregar una categoría y sus estilos

--- src/app/feature/views/category-list/category-list/category-list.component.ts
+++ src/app/feature/views/category-list/category-list/category-list.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class CategoryListComponent {
 
+
+  constructor(){}
+
+  /**
+   *  Metodo para crear y eliminar fila con los inputs y botones para una categoría
+   */
+  public addCategory(){
+    const tableBody = document.querySelector('.table-container tbody');
+    if(tableBody){
+      const newRow = document.createElement('tr');
+      newRow.className = 'row m-0';
+      newRow.innerHTML = 
+       `<td class="col-4">
+          <input type="text">
+        </td>
+        <td class="col-4">
+          <input type="text">
+        </td>
+        <td class="col-4">
+          <div class="row m-0">
+            <div class="col-6 p-0">
+              <button class="btn-action btn-save">Guardar</button>
+            </div>
+            <div class="col-6 p-0">
+              <button class="btn-action btn-delete">Eliminar</button>
+            </div>
+          </div>
+        </td>`;
+        tableBody.appendChild(newRow);
+
+        const deleteBtn = newRow.querySelector('.btn-delete');
+        if(deleteBtn){
+          deleteBtn.addEventListener('click', () => {
+            newRow.remove();
+          });
+        }
+    }
+  }
+
 }
